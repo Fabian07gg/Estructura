5014a7ea2ad66c415499e748d110edf79f12ca5a
Fabian07gg <fgarita40272@ufide.ac.cr>
2022-02-05 23:51:36 -0600

    Tarea Semana 2

---
+++ app/src/main/java/com/example/estructura/Pagina3.kt
@@ -0,0 +1,43 @@
+package com.example.estructura
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Pagina3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina3)
+        //boton1
+        val boton1 = findViewById<Button>(R.id.button8)
+        boton1.setOnClickListener{
+            val pase_vista = Intent(this, MainActivity::class.java)
+            startActivity(pase_vista)
+        }
+        //boton2
+        val boton2 = findViewById<Button>(R.id.button9)
+        boton2.setOnClickListener{
+            val pase_vista = Intent(this, Pagina2::class.java)
+            startActivity(pase_vista)
+        }
+        //boton3
+        val boton3 = findViewById<Button>(R.id.button10)
+        boton3.setOnClickListener{
+            val pase_vista = Intent(this, Pagina3::class.java)
+            startActivity(pase_vista)
+        }
+        //boton4
+        val boton4 = findViewById<Button>(R.id.button11)
+        boton4.setOnClickListener{
+            val pase_vista = Intent(this, Pagina4::class.java)
+            startActivity(pase_vista)
+        }
+        //boton5
+        val boton5 = findViewById<Button>(R.id.button12)
+        boton5.setOnClickListener{
+            val pase_vista = Intent(this, Pagina5::class.java)
+            startActivity(pase_vista)
+        }
+    }
+}
